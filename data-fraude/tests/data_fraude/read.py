ca66b78f71325940827b76cd3fd0a9e4a9482f75
Andreza <dedezasilva@live.com>
2024-09-03 19:15:04 -0300

    feat: criando filtragem diferente

--- data-fraude/tests/data_fraude/read.py
+++ data-fraude/tests/data_fraude/read.py
@@ -3,11 +3,32 @@ import pandas as pd
 dados = pd.read_csv('fraud.csv')
 df = pd.DataFrame(dados)
 
-contagem_agrupada = df.groupby('Sex')['Age'].count()
+contagem_agrupada_female = (
+	(df['Sex'] == 'Female') &
+	(df['Age'] > 20) &
+	(df['Age'] < 50) 
+)
+
+numero_de_registros = df[contagem_agrupada_female].shape[0]
+print(f"Quantidade por female entre 20 e 40 anos: {numero_de_registros}")
+
+print('-------------------------------------')
+
+contagem_agrupada_male = (
+	(df['Sex'] == 'Male') &
+	(df['Age'] > 20) &
+	(df['Age'] < 40) 
+
+)
+
+numero_de_registros = df[contagem_agrupada_male].shape[0]
+print(f"Quantidade por male entre 20 e 40 anos: {numero_de_registros}")
+print('-------------------------------------')
+
+
+
 contagem_veiculo = df['VehicleCategory'].value_counts()
-
-
-print(f'Contagem por genero', contagem_veiculo)
+print(f'Contagem por categoria de carro', contagem_veiculo)
 print('-------------------------------------')
 
 contagem_veiculos_envolvidos = df['NumberOfCars'].value_counts()
@@ -15,8 +36,5 @@ contagem_veiculos_envolvidos = df['NumberOfCars'].value_counts()
 print('Contagem de veiculos envolvidos', contagem_veiculos_envolvidos)
 print('-------------------------------------')
 
-filtro = df['WitnessPresent'] == 'Yes'
+# filtro = df['WitnessPresent'] == 'Yes'
 # quantidade_de_carros = df[filtro]['NumberOfCars'].sum()
-# print(f"Quantidade total de carros envolvidos em acidentes com testemunhas presentes: {quantidade_de_carros}")
-
-
--- data-fraude/tests/data_fraude/read_testemunhas.py
+++ data-fraude/tests/data_fraude/read_testemunhas.py
@@ -16,9 +16,24 @@ filtro_sex_male_veiculo_mais_caro_com_testemunha = (
 	(df['Age'] > 20) &
 	(df['Age'] < 50) & 
 	#(df['VehiclePrice'] == 'more than 69000') &
-	(df['VehicleCategory'] == 'Sedan') &
+	(df['VehicleCategory'] == 'Sport') &
+	(df['Month'] == 'Dec')
+)
+
+numero_de_registros = df[filtro_sex_male_veiculo_mais_caro_com_testemunha].shape[0]
+print(f"Numero: {numero_de_registros}")
+
+#Número de acidentes com testemunhas presentes com homens entre 20 e 50 anos e com veiculos Sedan no mês de Janeiro
+print('-------------------------------------')
+
+filtro_sex_male_veiculo_mais_caro_com_testemunha = (
+	(df['Sex'] == 'Male') &
+	(df['Age'] > 20) &
+	(df['Age'] < 50) & 
+	#(df['VehiclePrice'] == 'more than 69000') &
+	(df['VehicleCategory'] == 'Utility') &
 	(df['WitnessPresent'] == 'Yes')
 )
 
 numero_de_registros = df[filtro_sex_male_veiculo_mais_caro_com_testemunha].shape[0]
-print(f"Número de acidentes com testemunhas presentes com homens entre 20 e 50 anos e com veiculos caros: {numero_de_registros}")
+#print(f"Número de acidentes com testemunhas presentes com homens entre 20 e 50 anos e com veiculos caros: {numero_de_registros}")
