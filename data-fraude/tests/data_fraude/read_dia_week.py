de45134d1bbdeb9634e5407d1c2e509b63c95876
Andreza <dedezasilva@live.com>
2024-09-03 15:50:07 -0300

    feat: verificar a diferença da reclamação entre o dia do acidente e do dia que foi reportado

---
+++ data-fraude/tests/data_fraude/read_dia_week.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+dados = pd.read_csv('fraud.csv')
+df = pd.DataFrame(dados)
+
+# Criando um filtro para identificar registros onde o 'DayOfWeek' e o 'DayOfWeekClaimed' são incoerentes
+filtro_incoerente = df['DayOfWeek'] != df['DayOfWeekClaimed']
+
+# Aplicando o filtro para identificar os registros suspeitos
+registros_incoerentes = df[filtro_incoerente].shape[0]
+
+print('Criando um filtro para identificar registros onde o DayOfWeek e o DayOfWeekClaimed são incoerentes. Aplicando o filtro para identificar os registros suspeitos', registros_incoerentes)
